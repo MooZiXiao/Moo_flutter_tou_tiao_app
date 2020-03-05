772e0a2cf296f824a520221ca7e63a5d14cf4518
Zioo Moo <571869136@qq.com>
2020-03-05 20:44:08 +0800

    搭建项目

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MOO 头条',
+      home: Scaffold(
+        appBar: AppBar(
+          
+        ),
+      ),
+    );
+  }
+}
